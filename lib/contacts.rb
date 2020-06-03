d80aff2e255dbd67e49d7989dc847deb302413bf
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-03 01:07:23 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -2,7 +2,9 @@ require 'pry'
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-     if attribute == :favorite_ice_cream_flavors binding.pry
+     if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        binding.pry
     end
   end
 end
